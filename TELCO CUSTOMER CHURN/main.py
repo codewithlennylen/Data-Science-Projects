99a53508a5fa33439793053770d661a512fc0317
Eng. Lenny Ng'ang'a <33583893+codewithlennylen@users.noreply.github.com>
2020-11-12 15:47:01 +0300

    Datasets

---
+++ TELCO CUSTOMER CHURN/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("C:/Users/Lennylen/Documents/GitHub/Data-Science-Projects/TELCO CUSTOMER CHURN/data/customer.csv")
+
+
+print(data.columns) # Print the columns in the DataFrame
+print(data.shape) # Prints the rows x columns in the DataFrame.
+print(data.head())
+
+
