BUG : Radians to Degrees

DATA VISUALIZATION/Snell's Law Experiment/main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import math
-
+import numpy as np
 
 # A light ray from air entering water of refactive index, n = 1.33
 
@@ -8,10 +8,13 @@ import math
 n1 = 1 # Refractive index of air
 n2 = 1.33 # Refractive index of water
 
+## NOTE: The value returned by the math.sin function is in radians!!
+# 1 Radian = 57.2958 Degrees
+
 # Generate the angles of incidence with 5 degree increments
 theta_i = []
-start = 0
-for _i in range(30):
+start = 5
+for _i in range(10):
     theta_i.append(start)
     start += 5
 
@@ -24,4 +27,17 @@ print(theta_i[-1])
 # O2 = sin-1((n1sinO1/n2))
 theta_r = []
 for angle in theta_i:
-    calculated_theta_r = ((n1 * sin(angle))/n2)
+
+    calculated_theta_r = (math.sin(angle)) * 57.2958 # Convert radians to degrees
+    calculated_theta_r = ((n1 * math.sin(angle))/n2)
+    # calculated_theta_r = 1/math.sin(calculated_theta_r)
+    calculated_theta_r = math.asin(calculated_theta_r)
+
+    theta_r.append(calculated_theta_r)
+
+print(len(theta_r))
+print(theta_r[-1])
+
+print('\n\n')
+print(math.sin(50)*57.2958)
+print(np.sin(50)*57.2958)